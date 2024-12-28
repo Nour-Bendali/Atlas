45ac2610279892302261015d7d1ac9a9533559d3
KyzVin <139350196+KyzVin@users.noreply.github.com>
2024-12-28 20:54:23 +0100

    trying to use csv as source

---
+++ Load_db.sql
@@ -0,0 +1,44 @@
+
+COPY Regions (region_id, region_name, wikiDataId)
+FROM '/Users/elyazidbenkhadra/Desktop/ProjectAtlas/Atlas/datacsv/regions.csv'
+CSV HEADER;
+
+COPY Subregions (subregion_id, subregion_name, region_id, wikiDataId)
+FROM '/Users/elyazidbenkhadra/Desktop/ProjectAtlas/Atlas/datacsv/subregions.csv'
+CSV HEADER;
+
+COPY Countries (country_id, country_name, iso_code3, iso_code2, numeric_code, phone_code, capital, currency_code, currency_name, currency_symbol, native_name, region_id, subregion_id, nationality, timezones, latitude, longitude)
+FROM '/Users/elyazidbenkhadra/Desktop/ProjectAtlas/Atlas/datacsv/countries.csv'
+CSV HEADER;
+
+COPY States (state_id, state_name, country_id, state_code, state_type, latitude, longitude)
+FROM '/Users/elyazidbenkhadra/Desktop/ProjectAtlas/Atlas/datacsv/states.csv'
+CSV HEADER;
+
+COPY Cities (city_id, city_name, state_id, country_id, latitude, longitude, wikiDataId)
+FROM '/Users/elyazidbenkhadra/Desktop/ProjectAtlas/Atlas/datacsv/cities.csv'
+CSV HEADER;
+
+---------------------------------------------------------------- LOCAL LOAD
+-- Load Regions
+\COPY Regions (region_id, region_name, wikiDataId)
+FROM './datacsv/regions.csv'
+WITH (FORMAT csv, HEADER true);
+
+
+\COPY Subregions (subregion_id, subregion_name, region_id, WikiDataId)
+FROM './datacsv/subregions.csv'
+WITH (FORMAT csv, HEADER true);
+
+\COPY Countries (country_id, country_name, iso_code3, iso_code2, numeric_code, phone_code, capital, currency_code, currency_name, currency_symbol, native_name, region_id, subregion_id, nationality, timezones, latitude, longitude)
+FROM './datacsv/countries.csv'
+WITH (FORMAT csv, HEADER true);
+
+\COPY States (state_id, state_name, country_id, state_code, state_type, latitude, longitude)
+FROM './datacsv/states.csv'
+WITH (FORMAT csv, HEADER true);
+
+
+\COPY Cities (city_id, city_name, state_id, country_id, latitude, longitude, wikiDataId)
+FROM './datacsv/cities.csv'
+WITH (FORMAT csv, HEADER true);
---
+++ atlas_db.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Regions(
+    region_id INT PRIMARY KEY,
+    region_name VARCHAR(100) NOT NULL,
+    wikiDataId VARCHAR(20)
+);
+
+CREATE TABLE Subregions(
+    subregion_id INT PRIMARY KEY,
+    subregion_name VARCHAR(100) NOT NULL,
+    region_id INT NOT NULL,
+    wikidataID VARCHAR(20),
+    FOREIGN KEY (region_id) REFERENCES Regions(region_id)
+);
+
+CREATE TABLE Countries (
+    country_id INT PRIMARY KEY,
+    country_name VARCHAR(100) NOT NULL,
+    iso_code3 CHAR(3) NOT NULL,
+    iso_code2 CHAR(2) NOT NULL,
+    numeric_code INT NOT NULL,
+    phone_code INT NOT NULL,
+    capital VARCHAR(100),
+    currency_code VARCHAR(10),
+    currency_name VARCHAR(100),
+    currency_symbol VARCHAR(10),
+    native_name VARCHAR(100),
+    region_id INT NOT NULL,
+    subregion_id INT,
+    nationality VARCHAR(100),
+    timezones VARCHAR(200),
+    latitude DECIMAL(10, 6),
+    longitude DECIMAL(10, 6),
+    FOREIGN KEY (region_id) REFERENCES Regions(region_id),
+    FOREIGN KEY (subregion_id) REFERENCES Subregions(subregion_id)
+);
+
+CREATE TABLE States (
+    state_id INT PRIMARY KEY,
+    state_name VARCHAR(100) NOT NULL,
+    country_id INT NOT NULL,
+    state_code CHAR(5),
+    state_type VARCHAR(20),
+    latitude DECIMAL(10, 6),
+    longitude DECIMAL(10, 6),
+    FOREIGN KEY (country_id) REFERENCES Countries(country_id)
+);
+
+CREATE TABLE Cities (
+    city_id INT PRIMARY KEY,
+    city_name VARCHAR(100) NOT NULL,
+    state_id INT NOT NULL,
+    country_id INT NOT NULL,
+    latitude DECIMAL(10, 6),
+    longitude DECIMAL(10, 6),
+    wikiDataID VARCHAR(20),
+    FOREIGN KEY (state_id) REFERENCES States(state_id),
+    FOREIGN KEY (country_id) REFERENCES Countries(country_id)
+);
